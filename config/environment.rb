3c85c3377c273ddd54c2922a867479dee2eb6dbb
Miles Georgi <azimux@gmail.com>
2009-03-02 10:48:53 -0800

    automatically generate a fresh session key if needed

--- config/environment.rb
+++ config/environment.rb
@@ -22,13 +22,13 @@ Rails::Initializer.run do |config|
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Specify gems that this application depends on. 
+  # Specify gems that this application depends on.
   # They can then be installed with "rake gems:install" on new installations.
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -47,11 +47,18 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
+  # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
+  key_file = File.join(RAILS_ROOT, 'config', 'sess_key')
+  unless File.exists? key_file
+    File.open(key_file, "w") do |f|
+      f.write(ActiveSupport::SecureRandom.hex(64))
+    end
+  end
+
   config.action_controller.session = {
     :session_key => '_ax_forum_session',
-    :secret      => '3136ea6508a6dcae180b527b02f255461cb1777e2f2538934d117e87ae61f4635a81ea2e40699449fcd08009ef8d894127e207d163cb7337783f72e45e614d26'
+    :secret      => File.read(key_file).strip
   }
 
   # Use the database for sessions instead of the cookie-based default,
