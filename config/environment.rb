13366772eaefb631a42185230f4ae5815ca8b3cd
Miles Georgi <azimux@gmail.com>
2009-03-12 23:15:31 -0700

    connect the User class up with the wasabbi plugin

--- config/environment.rb
+++ config/environment.rb
@@ -73,4 +73,8 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+
+  config.after_initialize do
+    Wasabbi.user_class = User
+  end
 end
