a7d5e2f7abda97d5d3c909631c7735f262b14cff
Miles Georgi <azimux@gmail.com>
2009-03-14 14:37:06 -0700

    renamed ThreadListEntry to have Wasabbi infront like the other wasabbi_plugin models

--- test/unit/wasabbi_thread_list_entry_test.rb
+++ test/unit/wasabbi_thread_list_entry_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ThreadListEntryTest < ActiveSupport::TestCase
+class WasabbiThreadListEntryTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
